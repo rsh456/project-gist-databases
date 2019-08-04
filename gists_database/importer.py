c0d5809ce10c0a4fce0554dfbaf21e5d42d863e3
rsh456 <rsh456@hotmail.com>
2019-08-04 10:25:07 +0000

    Solved Gists databases

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,26 @@
 import requests
+import json
+import pytest
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    resp = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    if resp.status_code==404:
+        raise requests.HTTPError(resp.text)
+    resp_data=resp.json()
+    n=0
+    for i in resp_data:
+        dt_id= resp_data[n]["id"]
+        dt_url= resp_data[n]["html_url"]
+        dt_pllurl=resp_data[n]["git_pull_url"] 
+        dt_pshurl=resp_data[n]["git_push_url"] 
+        dt_commit =resp_data[n]["commits_url"] 
+        dt_frkurl=resp_data[n]["forks_url"]
+        dt_public=resp_data[n]["public"]
+        dt_create=resp_data[n]["created_at"]
+        dt_update=resp_data[n]["updated_at"]
+        dt_comment=resp_data[n]["comments"]
+        dt_cmturl=resp_data[n]["comments_url"]
+        if commit:
+            query= "INSERT INTO gists(github_id, html_url,git_pull_url, git_push_url, commits_url, forks_url, public, created_at, updated_at, comments,comments_url) VALUES (:dt_id,:dt_url,:dt_pllurl,:dt_pshurl,:dt_commit,:dt_frkurl,:dt_public,:dt_create,:dt_update,:dt_comment,:cmturl)"
+            db.execute(query,{'dt_id':dt_id,'dt_url':dt_url,'dt_pllurl':dt_pllurl,'dt_pshurl':dt_pshurl, 'dt_commit':dt_commit, 'dt_frkurl':dt_frkurl, 'dt_public':dt_public,'dt_create':dt_create,'dt_update':dt_update,'dt_comment':dt_comment,'cmturl':dt_cmturl})
+        n=n+1
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,24 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    cursor = None
+    #if not '**kwargs' in locals():
+        
+    if 'github_id' in kwargs:
+        params = [kwargs.get('github_id')]
+        query="SELECT * FROM gists WHERE github_id LIKE ?"
+        cursor=db_connection.execute(query,params)
+    elif 'created_at' in kwargs:
+        params = [kwargs.get('created_at')]
+        cursor = db_connection.execute("SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at)", params)
+    else:
+        query="SELECT * FROM gists"
+        cursor = db_connection.execute(query)
+    t_gist = cursor.fetchall()
+    v_lis =[]
+    n=0
+    for row in t_gist:
+        v_gist=Gist(t_gist[n])    
+        v_lis.append(v_gist)
+        m=n+1
+    return v_lis
